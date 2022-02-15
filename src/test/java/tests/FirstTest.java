5072a8dec4b64aee67dc22a3b51fac8b4576c9bf
Olga <63593441+OlGubkina@users.noreply.github.com>
2022-02-15 17:18:41 +0200

    Initial commit

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void sumTest () {
+        Assert.assertTrue("Message if condition = false", true);
+    }
+}
